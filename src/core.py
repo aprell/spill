c33057b1f4ad53ed5e6212653097b1c6b2563ff7
Andreas Prell <andreas.h.prell@gmail.com>
2023-07-31 18:57:49 +0200

    Define `__repr__` instead of `__str__`

--- src/core.py
+++ src/core.py
@@ -71,7 +71,7 @@ class Commands:
     def __len__(self):
         return len(self.cmds)
 
-    def __str__(self):
+    def __repr__(self):
         return f"[ {utils.concat(self.cmds)} ]"
 
 def parse(tokens, delim=None):
